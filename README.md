1d82fec7a2995ce598b81b0aa6161d9ff8b7845b
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-29 12:04:48 -0700

    add update for test number 1,2,3, and 4
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- README.md
+++ README.md
@@ -9,10 +9,13 @@ Expect: (toRoman (4000)).toEqual(alert("no numbers above 3900"));
 Test: "It will give number values to roman numeral symbols"
 Expect: (toRoman (1000)).toEqual("M");
 
-Test: "It will give specific values based on what it is divisible by"
-Expect: (toRoman (1000)).toEqual("M");
+Test: "It will give specific values based on what it is divisible by, based on subtractive method of if else statements""
+Expect: (toRoman (2000)).toEqual("M,D,C,L,X,V,I");
 
-Test: "It will gather more consice roman numeral symbol for user input, based on subtractive method of if else statements"
+Test: "It will join all pushed elements into a string"
+Expect: (toRoman (6)).toEqual ("VI");
+
+Test: "It will gather more consice roman numeral symbol for user input"
 Expect: (toRoman (2000)).toEqual("MM");
 
 Test: "It will give final roman numeral based on user input integer"
--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,116 @@
-
-let romanNumI = ["I", 1];
-let romanNumV = ["V", 5];
-if (userInput % 1000) {
-  userInput - 1000;
-  userOutput.concat("M");
-} else if (userInput % 500){
-  userInput - 500;
-  userOutput.concat("D");
-} else if (userInput % 100) {
-  userInput - 100;
-  userOutput.concat("C");
-} else if (userInput % 50) {
-  userInput - 50;
-  userOutput.concat("L");
-} else if (userInput % 10) {
-  userInput - 10;
-  userOutput.concat("X");
-} else if (userInput % 5) {
-  userInput - 5;
-  userOutput.concat("V");
-} else if (userInput % 1) {
-  userInput - 1;
-  userOutput.concat("I");
+function toRoman(userInputStr){
+  let userInput = parseInt(userInputStr);
+  userInput;
 }
 
 
-let userOutput = "";
+function toRoman(userInputStr){
+  let userInput = parseInt(userInputStr);
+  userInput;
+  if (userInput >3900) {
+    alert("No numbers over 3,900!!!")
+  }
+}
+
+
+function toRoman(userInputStr){
+  let userInput = parseInt(userInputStr);
+  if (userInput >3900) {
+    alert("No numbers over 3,900!!!")
+  }
+  userOutput = [];
+  if (userInput % 1000 === 0) {
+    userOutput.push('M');
+    alert(userOutput);
+  }
+}
+
+
+function toRoman(userInputStr){
+  let userInput = parseInt(userInputStr);
+  if (userInput >3900) {
+    alert("No numbers over 3,900!!!")
+  }
+  userOutput = [];
+  if (userInput % 1000 === 0) {
+    userInput - 1000;
+    userOutput.push('M');
+  } 
+  if (userInput % 500 === 0){
+    userInput - 500;
+    userOutput.push('D');
+  }  
+  if (userInput % 100 === 0) {
+    userInput - 100;
+    userOutput.push('C');
+  }  
+  if (userInput % 50 === 0) {
+    userInput - 50;
+    userOutput.push('L');
+  }  
+  if (userInput % 10 === 0) {
+    userInput - 10;
+    userOutput.push('X');
+  }  
+  if (userInput % 5 === 0) {
+    userInput - 5;
+    userOutput.push('V');
+  } 
+  if (userInput % 1 === 0) {
+    userInput - 1;
+    userOutput.push('I');
+  }
+    alert(userOutput);
+}
+
+
+function toRoman(userInputStr){
+  let userInput = parseInt(userInputStr);
+  if (userInput >3900) {
+    alert("No numbers over 3,900!!!")
+  }
+  userOutput = [];
+  if (userInput % 1000 === 0) {
+    userInput - 1000;
+    userOutput.push('M');
+  } 
+  if (userInput % 500 === 0){
+    userInput - 500;
+    userOutput.push('D');
+  }  
+  if (userInput % 100 === 0) {
+    userInput - 100;
+    userOutput.push('C');
+  }  
+  if (userInput % 50 === 0) {
+    userInput - 50;
+    userOutput.push('L');
+  }  
+  if (userInput % 10 === 0) {
+    userInput - 10;
+    userOutput.push('X');
+  }  
+  if (userInput % 5 === 0) {
+    userInput - 5;
+    userOutput.push('V');
+  } 
+  if (userInput % 1 === 0) {
+    userInput - 1;
+    userOutput.push('I');
+  }
+    
+    alert(userOutput);
+}
+
+
+  
+  
+  
+  
+
+
+
+
+
+
+
